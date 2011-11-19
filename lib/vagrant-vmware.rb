c15fb291b0af25a89e5623ec7bcf138065cb6604
Barry Allard <barry@barryallard.name>
2011-11-18 17:59:30 -0800

    added vagrant-vmware.rb

---
+++ lib/vagrant-vmware.rb
@@ -0,0 +1,17 @@
+require 'vagrant'
+# require 'vagrant-vmware/config'
+# require 'vagrant-vmware/command'
+# require 'vagrant-vmware/middleware'
+
+# Create a new middleware stack "vmware" which is executed for
+# vmware commands. See the VagrantRake::Middleware docs for more
+# information.
+# vmware = Vagrant::Action::Builder.new do
+#   use VagrantVMware::Middleware
+# end
+
+# Vagrant::Action.register(:rake, vmware)
+
+# Add our custom translations to the load path
+# I18n.load_path << File.expand_path("../../locales/en.yml", __FILE__)
+
